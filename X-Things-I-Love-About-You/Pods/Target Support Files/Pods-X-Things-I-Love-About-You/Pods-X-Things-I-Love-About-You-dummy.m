8f982f6e97898e3ba10dbafd84c4472ef1ad44f8
thomas.kellough <thomas.r.kellough@gmail.com>
2021-01-24 16:44:00 -0700

    added cocoapod for tabbar

---
+++ X-Things-I-Love-About-You/Pods/Target Support Files/Pods-X-Things-I-Love-About-You/Pods-X-Things-I-Love-About-You-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_X_Things_I_Love_About_You : NSObject
+@end
+@implementation PodsDummy_Pods_X_Things_I_Love_About_You
+@end
---
+++ X-Things-I-Love-About-You/Pods/Target Support Files/Pods-X-Things-I-Love-About-You/Pods-X-Things-I-Love-About-You-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_X_Things_I_Love_About_YouVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_X_Things_I_Love_About_YouVersionString[];
+
